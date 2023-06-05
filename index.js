a6419d3db212479d9860d7b6a680881036771e3e
Neeraj Kumar <neerajkmahawar2017@gmail.com>
2023-06-06 01:48:21 +0530

    fix: change message format

--- index.js
+++ index.js
@@ -5,15 +5,21 @@ const axios = require("axios");
 async function run() {
   try {
     const owner = core.getInput("owner", { required: true });
+
     const repo = core.getInput("repo", { required: true });
+
     const pr_number = core.getInput("pr_number", { required: true });
+
     const github_api_key = core.getInput("github_api_key", { required: true });
+
     const discord_webhook_url = core.getInput("discord_webhook_url", {
       required: false,
     });
+
     const telegram_chat_id = core.getInput("telegram_chat_id", {
       required: false,
     });
+
     const telegram_bot_token = core.getInput("telegram_bot_token", {
       required: telegram_chat_id ? true : false,
     });
@@ -26,41 +32,36 @@ async function run() {
       pull_number: pr_number,
     });
 
+    if (pull_request?.user?.login?.toLowerCase().includes("dependabot[bot]")) {
+      return;
+    }
+
+    if (pull_request?.title?.toLowerCase().includes("wip")) {
+      return;
+    }
+
     const sendMessage = [];
 
-    sendMessage.push(`Project: ${pull_request.base.repo.name}`);
-    sendMessage.push("");
-
-    sendMessage.push(`Creator: ${pull_request.assignee.login}`);
-
-    sendMessage.push(
-      `Created At: ${new Date(pull_request.created_at).toLocaleString()}`
-    );
-
-    sendMessage.push(
-      `Updated At: ${new Date(pull_request.updated_at).toLocaleString()}`
-    );
-    sendMessage.push("");
-
-    sendMessage.push(`Link: ${pull_request.html_url}`);
-
-    sendMessage.push("");
-
-    let reviewers = "Reviewers : ";
+    sendMessage.push(`🔥 New Pull Request Alert!`);
+    sendMessage.push(`Project: ${pull_request?.base?.repo.name}`);
+    sendMessage.push(`Title: ${pull_request?.title}`);
+    sendMessage.push(`Author: ${pull_request?.user.login}`);
+    let reviewers = "Reviewers: ";
 
     for (const reviewer of pull_request.requested_reviewers) {
       reviewers += reviewer.login + ", ";
     }
 
     sendMessage.push(reviewers);
+    sendMessage.push(`Link: ${pull_request?.html_url}`);
 
     // Send message to discord
     if (discord_webhook_url) {
       const discordText = sendMessage.join("\n");
-      await axios.post(
+      axios.post(
         discord_webhook_url,
         {
-          username: "Neeraj Kumar",
+          username: "Neeraj",
           content: discordText,
         },
         {
@@ -73,10 +74,12 @@ async function run() {
 
     // Send message to telegram
     if (telegram_bot_token && telegram_chat_id) {
-      const telegramText = sendMessage.join("%0A");
-      const telegramApiUrl = `https://api.telegram.org/bot${telegram_bot_token}/sendMessage?chat_id=${telegram_chat_id}&text=${telegramText}`;
+      const telegramText = sendMessage.join("\n");
+      const telegramApiUrl = `https://api.telegram.org/bot${telegram_bot_token}/sendMessage?chat_id=${telegram_chat_id}&text=${encodeURIComponent(
+        telegramText
+      )}`;
 
-      await axios.get(telegramApiUrl);
+      axios.get(telegramApiUrl);
     }
   } catch (error) {
     core.setFailed(error.message);
